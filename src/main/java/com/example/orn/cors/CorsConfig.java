0f795a97dfa322a9b25b4eee639997541bdd03c4
arundeveloper2731 <arunrajuka2001@gmail.com>
2026-07-08 12:17:34 +0530

    /

--- src/main/java/com/example/orn/cors/CorsConfig.java
+++ src/main/java/com/example/orn/cors/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://manageorn.netlify.app/")
+                        .allowedOrigins("https://manageorn.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         // Required so the browser sends/receives the JSESSIONID
